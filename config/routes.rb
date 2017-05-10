8ba5f9d73a3832ddb7107a527421d2a4602408ff
kashkama <owen.kamau2014@gmail.com>
2017-05-10 16:13:52 +0300

    adding styling to landing page and skill page, adding all the relevant links to projects

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,16 @@ Rails.application.routes.draw do
   get 'comments/new'
 
   devise_for :users
-  devise_for :admins, skip: [:registrations]
+  devise_for :admins, skip: [:registrations, :sessions]
   as :admin do
     get 'signupforprint920120e9210weq0e0e12e19812928', to: 'devise/registrations#new', as: :new_admin_registration
+    get 'signin34939v23223n3' => 'devise/sessions#new', :as => :new_admin_session
+    delete 'signout' => 'devise/sessions#destroy', :as => :destroy_admin_session
+    post 'signinjkwr9242' => 'devise/sessions#create', :as => :admin_session
     post 'signinforprint', to: 'devise/registrations#create', as: :admin_registration
     delete 'signout', to: 'devise/registrations#destroy', as: :destroy_admin_registration
   end
+
 
   root :to => "home#index"
 
